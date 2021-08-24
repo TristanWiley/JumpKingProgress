020e7afac50c83f6d5f9f72a6047131641694a46
Tristan Wiley <triswile@twitch.tv>
2021-08-24 00:15:36 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const img = document.getElementById("map");
+let freeze = false;
+
+img.onmousemove = (e) => {
+    if (freeze) return;
+    const clickY = e.pageY - img.offsetTop;
+
+    const percentYImg = clickY * 100 / img.height;
+    const progress = Math.round(((100-percentYImg) + Number.EPSILON) * 100) / 100;
+    document.getElementById("percentage").innerText = Math.max(0, Math.min(progress, 100)) + "%";
+}
+
+img.onclick = () => {
+    freeze = !freeze;
+
+    document.getElementById("frozen").innerText = freeze ? "unfreeze" : "freeze";
+}
