17047290d617938135c773d4246376a3ed0fe18b
Tristan Wiley <triswile@twitch.tv>
2021-08-24 00:20:35 -0700

    Scroll to bottom on load

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
+window.scrollTo(0, document.body.scrollHeight);
+
 const img = document.getElementById("map");
 let freeze = false;
 
 img.onmousemove = (e) => {
-    if (freeze) return;
-    const clickY = e.pageY - img.offsetTop;
+  if (freeze) return;
+  const clickY = e.pageY - img.offsetTop;
 
-    const percentYImg = clickY * 100 / img.height;
-    const progress = Math.round(((100-percentYImg) + Number.EPSILON) * 100) / 100;
-    document.getElementById("percentage").innerText = Math.max(0, Math.min(progress, 100)) + "%";
-}
+  const percentYImg = (clickY * 100) / img.height;
+  const progress = Math.round((100 - percentYImg + Number.EPSILON) * 100) / 100;
+  document.getElementById("percentage").innerText =
+    Math.max(0, Math.min(progress, 100)) + "%";
+};
 
 img.onclick = () => {
-    freeze = !freeze;
+  freeze = !freeze;
 
-    document.getElementById("frozen").innerText = freeze ? "unfreeze" : "freeze";
-}
+  document.getElementById("frozen").innerText = freeze ? "unfreeze" : "freeze";
+};
