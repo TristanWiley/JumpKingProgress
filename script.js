4fe968a9c79b71853e4b6ea26677893b0d7f1b88
Tristan Wiley <triswile@twitch.tv>
2021-08-25 00:15:34 -0700

    Add multiple maps with cookies for storing

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
-window.scrollTo(0, document.body.scrollHeight);
-
-window.onbeforeunload = function () {
-    window.scrollTo(0, document.body.scrollHeight);
-};
-
 const img = document.getElementById("map");
 let freeze = false;
+
+const maps = {
+  original: {
+    displayName: "Original",
+    image: "map.jpeg",
+  },
+  nbplus: {
+    displayName: "New Babe+",
+    image: "nbplus.jpeg",
+  },
+};
+
+let currentMap = getCookie("CURRENT_MAP") || "original";
+
+updateMap(currentMap);
+
+document.getElementById("maps").innerHTML = Object.entries(maps)
+  .map(
+    ([id, details]) =>
+      `<option ${id === currentMap ? "selected" : ""} value="${id}">${
+        details.displayName
+      }</option>`
+  )
+  .join("");
+
+document.getElementById("maps").onchange = (e) => {
+  setCookie("CURRENT_MAP", e.target.value, 360);
+  updateMap(e.target.value);
+};
 
 img.onmousemove = (e) => {
   if (freeze) return;
@@ -22,3 +45,34 @@ img.onclick = () => {
 
   document.getElementById("frozen").innerText = freeze ? "unfreeze" : "freeze";
 };
+
+function updateMap(mapID) {
+  document.getElementById("map").src = maps[mapID].image;
+  document.getElementById("percentage").innerText = "Hover on image";
+  freeze = false;
+  document.getElementById("frozen").innerText = "freeze";
+}
+
+// Cookie helper functions
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
